c4fac0d8fbfbbc2da67f132b32740ab11ff21e96
HarryYen <harry0109@gmail.com>
2025-05-28 15:53:27 +0800

    update: keep all the same

--- WaveSimModelGenerator/model_main_2d.py
+++ WaveSimModelGenerator/model_main_2d.py
@@ -54,31 +54,32 @@ def main():
     # --------------------------------------------------------- #
     # -------------specify your own structures----------------- #
     # --------------------------------------------------------- #
-    synmodel_creator.set_up_fault_zone(surface_x = 5,
-                                       dip_angle = 10, 
-                                       deepest_z = 999., 
-                                       app_thick = 999., 
-                                       specified_value = 4., 
+    synmodel_creator.set_up_fault_zone(surface_x = -20,
+                                       dip_angle = 20, 
+                                       deepest_z = 40., 
+                                       app_thick = 10., 
+                                       specified_value = 8., 
                                        percentage_flag = False)
+    
     ### create a rectangle area
     # High-velocity boundary
     synmodel_creator.set_up_rectangle(
-        x1 = 16, z1 = 20, x2 = 9999, z2 = 20, x3 = 9999, z3 = 9999, x4 = 16, z4 = 9999,  
-        new_value = 7.2, percentage_flag = True
+        x1 = -20, z1 = 20, x2 = 20, z2 = 20, x3 = 20, z3 = 40, x4 = -20, z4 = 40,  
+        new_value = 7.2, percentage_flag = False
     )
 
-    irregular_x = np.array([-15,  13., 12.5,    8,     5,     5,   -14, -16,  -14])
-    irregular_z = np.array([42.5, 42.5, 60.5, 67.5,  75.5, 98.5, 98.5,  60, 42.5])
-    irregular_z = irregular_z - 17.5
-    points = list(zip(irregular_x, irregular_z))
-    synmodel_creator.set_up_irregular_zone(points=points, 
-                                           new_value=11.3, percentage_flag=True, 
-                                           smooth_sigma=10, num_interpolated_points=100,
-                                           s=1000, k=3)
+    # irregular_x = np.array([-15,  13., 12.5,    8,     5,     5,   -14, -16,  -14])
+    # irregular_z = np.array([42.5, 42.5, 60.5, 67.5,  75.5, 98.5, 98.5,  60, 42.5])
+    # irregular_z = irregular_z - 17.5
+    # points = list(zip(irregular_x, irregular_z))
+    # synmodel_creator.set_up_irregular_zone(points=points, 
+    #                                        new_value=11.3, percentage_flag=True, 
+    #                                        smooth_sigma=10, num_interpolated_points=100,
+    #                                        s=1000, k=3)
    
    
     # Gaussian to blur the boundary
-    synmodel_creator.blur_model(mode='nearest', sigma=50)
+    synmodel_creator.blur_model(mode='nearest', sigma=5)
     # --------------------------------------------------------- #
     # -----------------------Post-Processing------------------- #
     # --------------------------------------------------------- #
